d0c7f5fb46698fa5cba13bbc8478a39a3a269ceb
Adrian-Muino <101266297+Adrian-Muino@users.noreply.github.com>
2023-04-18 11:45:22 +0200

    Add files via upload

--- code/m5stack/transcribe_function_core2_that_works.py
+++ code/m5stack/transcribe_function_core2_that_works.py
@@ -1,3 +1,5 @@
+######### Imports #########
+
 from m5stack import *
 from m5stack_ui import *
 from uiflow import *
@@ -5,17 +7,26 @@ from m5stack import mic
 import urequests
 import base64
 
+######### Default settings (screen & lateral light) #########
+
 screen = M5Screen()
 screen.clean_screen()
 screen.set_screen_bg_color(0xf0fcff)
+rgb.setColorAll(0x33ff33)
+
+######### Buttons & labels in the screen #########
 
 record_button = M5Btn(text='Record', x=0, y=60, w=80, h=180, bg_c=0x601a1a, text_c=0xffffff, font=FONT_MONT_18, parent=None)
 repeat_button = M5Btn(text='Repeat', x=88, y=132, w=200, h=50, bg_c=0xd2ab6f, text_c=0x000000, font=FONT_MONT_18, parent=None)
 transcription_output = M5Label('transcribed_text', x=5, y=20, color=0x000, font=FONT_MONT_10, parent=None)
 translate_button = M5Btn(text='Translate', x=88, y=190, w=210, h=50, bg_c=0x3f804e, text_c=0xffffff, font=FONT_MONT_22, parent=None)
 
+######### Gloabl variables #########
+
 translation_text = None
-rgb.setColorAll(0x33ff33)
+
+
+######### Record button pressed method #########
 
 def record_button_pressed():
   # global params
@@ -25,6 +36,8 @@ def record_button_pressed():
 
 record_button.pressed(record_button_pressed)
 
+######### Repeat button pressed method #########
+
 def repeat_button_pressed():
   # global params
   speaker.playWAV('/flash/sound1.wav', volume=6)
@@ -32,7 +45,17 @@ def repeat_button_pressed():
 
 repeat_button.pressed(repeat_button_pressed)
 
+######### Translate button pressed method #########
+
 def translate_button_pressed():
+    transcribe_wav()
+    assign_result_to_translation_text()
+
+translate_button.pressed(translate_button_pressed) 
+
+######### Transcribe method using Cloud #########
+
+def transcribe_wav():
   # Set the API key and language code
   rgb.setColorAll(0x08deff)
   
@@ -68,19 +91,17 @@ def translate_button_pressed():
       transcription = response.json()['results'][0]['alternatives'][0]['transcript']
       global translation_text
       translation_text = transcription
-      assign_transcription_to_translation_text()
     else:
       global translation_text
       translation_text = "Error"
-      assign_transcription_to_translation_text()
   except Exception as e:
     transcription_output.set_text(str(e))
   rgb.setColorAll(0x33ff33)
-translate_button.pressed(translate_button_pressed)  
+ 
   
+######### Assign result to translation label method #########
 
-
-
-def assign_transcription_to_translation_text():
+def assign_result_to_translation_text():
   global translation_text
   transcription_output.set_text(str(translation_text))
+
