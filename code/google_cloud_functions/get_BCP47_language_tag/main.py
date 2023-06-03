6d78b0bda980d26ec44c6bae050b1e0593869129
Adrian-Muino <101266297+Adrian-Muino@users.noreply.github.com>
2023-06-03 20:35:36 +0200

    update

--- code/google_cloud_functions/get_BCP47_language_tag/main.py
+++ code/google_cloud_functions/get_BCP47_language_tag/main.py
@@ -1,11 +1,10 @@
-from langcodes import Language
-import json
+import langcodes
 
 def get_BCP47_language_tag(request):
-    request_json = request.get_json()
-    language_name = request_json['language_name']
     try:
-        lang = Language.find(language_name)
-        return json.dumps({'bcp47_language_tag': lang.bcp47})
-    except:
-        return json.dumps({'bcp47_language_tag': None})
+        language_input = request.args.get('language_input', '').lower()
+        language = langcodes.find(language_input)
+        bcp47_language_tag = language.language
+        return bcp47_language_tag
+    except Exception as e:
+        return f"Error: {str(e)}"
